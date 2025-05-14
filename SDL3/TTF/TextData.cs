3525cb8265e2317e7910d11d48ab52060eec7f94
Adonis S. Deliannis <blizzardo1@blizzeta.net>
2025-05-13 23:08:36 -0700

    * Filled TextData

--- SDL3/TTF/TextData.cs
+++ SDL3/TTF/TextData.cs
@@ -1,4 +1,7 @@
-﻿namespace SharpSDL3.TTF;
+﻿using SharpSDL3.Structs;
+using System.Runtime.InteropServices;
+
+namespace SharpSDL3.TTF;
 
 public static unsafe partial class Ttf {
     /**
@@ -6,5 +9,26 @@ public static unsafe partial class Ttf {
      *
      * \since This struct is available since SDL_ttf 3.0.0.
      */
-    public struct TextData { }
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct TextData {
+        public Font* Font;             /**< The font used by this text, read-only. */
+        public FColor Color;           /**< The color of the text, read-only. */
+
+        public bool NeedsLayoutUpdate;   /**< True if the layout needs to be updated */
+        public nint Layout;     /**< Cached layout information, read-only. */
+        public int X;                      /**< The x offset of the upper left corner of this text, in pixels, read-only. */
+        public int Y;                      /**< The y offset of the upper left corner of this text, in pixels, read-only. */
+        public int W;                      /**< The width of this text, in pixels, read-only. */
+        public int H;                      /**< The height of this text, in pixels, read-only. */
+        public int NumOps;                /**< The number of drawing operations to render this text, read-only. */
+        public nint Ops;     /**< The drawing operations used to render this text, read-only. */
+        public int NumClusters;           /**< The number of substrings representing clusters of glyphs in the string, read-only */
+        public SubString* Clusters;    /**< Substrings representing clusters of glyphs in the string, read-only */
+
+        public int Props;     /**< Custom properties associated with this text, read-only. This field is created as-needed using TTF_GetTextProperties() and the properties may be then set and read normally */
+
+        public bool NeedsEngineUpdate;   /**< True if the engine text needs to be updated */
+        public nint Engine;     /**< The engine used to render this text, read-only. */
+        public nint EngineText;          /**< The implementation-specific representation of this text */
+    };
 }
