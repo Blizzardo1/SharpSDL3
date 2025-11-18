f3a64ec9bbbcfeae2b74cf715b015135ba4d412a
Adonis S. Deliannis <blizzardo1@blizzeta.net>
2025-11-18 14:55:26 -0800

    + Added documentation to ScaleMode.cs + Added documentation to remaining functions in Logger.cs * Cleaned up and properly formatted documentation to Logger.cs * Removed Unsafe.Copy and replaced with Marshal Interop Functions in Sdl.cs

--- SDL3/Logger.cs
+++ SDL3/Logger.cs
@@ -7,7 +7,7 @@ using static SharpSDL3.Delegates;
 
 namespace SharpSDL3;
 
-public static unsafe partial class Sdl {
+public static partial class Sdl {
     /// <summary>Get the default log output function.</summary>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
@@ -15,15 +15,13 @@ public static unsafe partial class Sdl {
     /// <seealso cref="SetLogOutputFunction"/>
     /// <seealso cref="GetLogOutputFunction"/>
     /// </remarks>
-    /// <returns>Returns the default logoutput callback.</returns>
-
+    /// <returns>Returns the default log output callback.</returns>
     public static SdlLogOutputFunction GetDefaultLogOutputFunction() {
         SdlLogOutputFunction? callback = SDL_GetDefaultLogOutputFunction();
         return callback ?? throw new InvalidOperationException("Failed to retrieve default log output function.");
     }
 
     /// <summary>Get the current log output function.</summary>
-
     /// <param name="callback">an SDL_LogOutputFunction filled in with the current log callback.</param>
     /// <param name="userdata">a pointer filled in with the pointer that is passed to callback.</param>
     /// <remarks>
@@ -32,7 +30,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="GetDefaultLogOutputFunction"/>
     /// <seealso cref="SetLogOutputFunction"/>
     /// </remarks>
-
     public static void GetLogOutputFunction(out SdlLogOutputFunction callback, out nint userdata) {
         SDL_GetLogOutputFunction(out callback, out userdata);
         if (callback == null) {
@@ -41,7 +38,6 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Get the priority of a particular log category.</summary>
-
     /// <param name="category">the category to query.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
@@ -49,14 +45,28 @@ public static unsafe partial class Sdl {
     /// <seealso cref="SetLogPriority"/>
     /// </remarks>
     /// <returns>Returns theSDL_LogPriority for the requested category.</returns>
-
     public static LogPriority GetLogPriority(LogCategory category) {
-        if (category == 0) {
-            throw new ArgumentOutOfRangeException(nameof(category), "Invalid log category.");
-        }
-        return SDL_GetLogPriority(category);
+        return category == 0 ? throw new ArgumentOutOfRangeException(nameof(category), "Invalid log category.") : SDL_GetLogPriority(category);
     }
 
+    /// <summary>
+    /// Log a message with <see cref="LogCategory.Application"/> and <see cref="LogPriority.Info"/>.
+    /// </summary>
+    /// <param name="fmt">An interpolated formatted string.</param>
+    /// <remarks>
+    /// <para><strong>Thread Safety</strong>: It is safe to call this function on any thread.</para>
+    /// <para><strong>Version</strong>: This function is available since SDL 3.2.0.</para>
+    /// </remarks>
+    /// <seealso cref="LogCritical"/>
+    /// <seealso cref="LogDebug"/>
+    /// <seealso cref="LogError"/>
+    /// <seealso cref="LogInfo"/>
+    /// <seealso cref="LogMessage"/>
+    /// <seealso cref="LogMessageV"/>
+    /// <seealso cref="LogTrace"/>
+    /// <seealso cref="LogVerbose"/>
+    /// <seealso cref="LogWarn"/>
+    /// <exception cref="ArgumentException"></exception>
     public static void Log(string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -66,10 +76,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_CRITICAL.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -83,7 +91,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogCritical(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -92,9 +99,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_DEBUG.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
@@ -109,7 +115,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogDebug(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -118,10 +123,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_ERROR.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -135,7 +138,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogError(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -144,9 +146,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_INFO.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
@@ -161,7 +162,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogInfo(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -170,11 +170,9 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with the specified category and priority.</summary>
-
     /// <param name="category">the category of the message.</param>
     /// <param name="priority">the priority of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -188,7 +186,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogMessage(LogCategory category, LogPriority priority, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -197,10 +194,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_TRACE.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -215,7 +210,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogVerbose"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogTrace(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -224,10 +218,8 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Log a message with SDL_LOG_PRIORITY_VERBOSE.</summary>
-
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -240,7 +232,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogMessageV"/>
     /// <seealso cref="LogWarn"/>
     /// </remarks>
-
     public static void LogVerbose(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -251,8 +242,7 @@ public static unsafe partial class Sdl {
     /// <summary>Log a message with SDL_LOG_PRIORITY_WARN.</summary>
 
     /// <param name="category">the category of the message.</param>
-    /// <param name="fmt">a printf() style message format string.</param>
-    /// <param name="...">additional parameters matching % tokens in the fmt string, if any.</param>
+    /// <param name="fmt">An interpolated formatted string.</param>
     /// <remarks>
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -266,7 +256,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="LogTrace"/>
     /// <seealso cref="LogVerbose"/>
     /// </remarks>
-
     public static void LogWarn(LogCategory category, string fmt) {
         if (string.IsNullOrWhiteSpace(fmt)) {
             throw new ArgumentException("Log message cannot be null or empty.", nameof(fmt));
@@ -282,13 +271,11 @@ public static unsafe partial class Sdl {
     /// <seealso cref="SetLogPriorities"/>
     /// <seealso cref="SetLogPriority"/>
     /// </remarks>
-
     public static void ResetLogPriorities() {
         SDL_ResetLogPriorities();
     }
 
     /// <summary>Replace the default log output function with one of your own.</summary>
-
     /// <param name="callback">an SDL_LogOutputFunction to call instead of the default.</param>
     /// <param name="userdata">a pointer that is passed to callback.</param>
     /// <remarks>
@@ -297,7 +284,6 @@ public static unsafe partial class Sdl {
     /// <seealso cref="GetDefaultLogOutputFunction"/>
     /// <seealso cref="GetLogOutputFunction"/>
     /// </remarks>
-
     public static void SetLogOutputFunction(SdlLogOutputFunction callback, nint userdata) {
         if (callback == null) {
             throw new ArgumentNullException(nameof(callback), "Log output function cannot be null.");
@@ -305,15 +291,25 @@ public static unsafe partial class Sdl {
         SDL_SetLogOutputFunction(callback, userdata);
     }
 
+    /// <summary>
+    /// Set the priority of all log categories.
+    /// </summary>
+    /// <param name="priority">the <see cref="LogPriority"/> to assign</param>
+    /// <remarks>
+    /// <para><strong>Thread Safety</strong>: It is safe to call this function from any thread.</para>
+    /// <para><strong>Version</strong>: This function is available since SDL 3.2.0</para>
+    /// </remarks>
+    /// <seealso cref="ResetLogPriorities"/>
+    /// <seealso cref="SetLogPriority"/>
+    /// <exception cref="ArgumentOutOfRangeException">If an invalid <see cref="LogPriority"/> value is assigned. This is typically thrown if a cast from <see cref="int"/> is passed.</exception>
     public static void SetLogPriorities(LogPriority priority) {
-        if (priority < LogPriority.Invalid || priority > LogPriority.Count) {
+        if (priority is < LogPriority.Invalid or > LogPriority.Count) {
             throw new ArgumentOutOfRangeException(nameof(priority), "Invalid log priority.");
         }
         SDL_SetLogPriorities(priority);
     }
 
     /// <summary>Set the priority of a particular log category.</summary>
-
     /// <param name="category">the category to assign a priority to.</param>
     /// <param name="priority">the SDL_LogPriority to assign.</param>
     /// <remarks>
@@ -323,24 +319,22 @@ public static unsafe partial class Sdl {
     /// <seealso cref="ResetLogPriorities"/>
     /// <seealso cref="SetLogPriorities"/>
     /// </remarks>
-
+    /// <exception cref="ArgumentOutOfRangeException">If an invalid <see cref="LogPriority"/> value is assigned. This is typically thrown if a cast from <see cref="int"/> is passed.</exception>
     public static void SetLogPriority(LogCategory category, LogPriority priority) {
         if (category == 0) {
             throw new ArgumentOutOfRangeException(nameof(category), "Invalid log category.");
         }
-        if (priority < LogPriority.Invalid || priority > LogPriority.Count) {
+        if (priority is < LogPriority.Invalid or > LogPriority.Count) {
             throw new ArgumentOutOfRangeException(nameof(priority), "Invalid log priority.");
         }
         SDL_SetLogPriority(category, priority);
     }
 
     /// <summary>Set the text prepended to log messages of a given priority.</summary>
-
     /// <param name="priority">the SDL_LogPriority to modify.</param>
-    /// <param name="prefix">the prefix to use for that log priority, or <see langword="null" /> to use no prefix.</param>
     /// <remarks>
-    /// By default SDL_LOG_PRIORITY_INFO and below have no
-    /// prefix, and SDL_LOG_PRIORITY_WARN and higher have
+    /// By default, <see cref="LogPriority.Info"/> and below have no
+    /// prefix, and <see cref="LogPriority.Warn"/> and higher have
     /// a prefix showing their priority, e.g. &quot;WARNING: &quot;.
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -348,9 +342,9 @@ public static unsafe partial class Sdl {
     /// <seealso cref="SetLogPriority"/>
     /// </remarks>
     /// <returns>Returns <see langword="true" /> on success or <see langword="false" /> on failure; call <see cref="GetError()"/> for more information.</returns>
-
+    /// <exception cref="ArgumentOutOfRangeException">If an invalid <see cref="LogPriority"/> value is assigned. This is typically thrown if a cast from <see cref="int"/> is passed.</exception>
     public static void SetLogPriorityPrefix(LogPriority priority) {
-        if (priority < LogPriority.Invalid || priority > LogPriority.Count) {
+        if (priority is < LogPriority.Invalid or > LogPriority.Count) {
             throw new ArgumentOutOfRangeException(nameof(priority), "Invalid log priority.");
         }
 
@@ -359,12 +353,11 @@ public static unsafe partial class Sdl {
     }
 
     /// <summary>Set the text prepended to log messages of a given priority.</summary>
-
-    /// <param name="priority">the SDL_LogPriority to modify.</param>
+    /// <param name="priority">the <see cref="LogPriority"/> to modify.</param>
     /// <param name="prefix">the prefix to use for that log priority, or <see langword="null" /> to use no prefix.</param>
     /// <remarks>
-    /// By default SDL_LOG_PRIORITY_INFO and below have no
-    /// prefix, and SDL_LOG_PRIORITY_WARN and higher have
+    /// By default, <see cref="LogPriority.Info"/> and below have no
+    /// prefix, and <see cref="LogPriority.Warn"/> and higher have
     /// a prefix showing their priority, e.g. &quot;WARNING: &quot;.
     /// <para><strong>Thread Safety:</strong> It is safe to call this function from any thread.</para>
     /// <para><strong>Version:</strong> This function is available since SDL 3.2.0.</para>
@@ -372,11 +365,7 @@ public static unsafe partial class Sdl {
     /// <seealso cref="SetLogPriority"/>
     /// </remarks>
     /// <returns>Returns <see langword="true" /> on success or <see langword="false" /> on failure; call <see cref="GetError()"/> for more information.</returns>
-
     public static void SetLogPriorityPrefix(LogPriority priority, string prefix) {
-        if (string.IsNullOrWhiteSpace(prefix)) {
-            throw new ArgumentException("Prefix cannot be null or empty.", nameof(prefix));
-        }
         SDL_SetLogPriorityPrefix(priority, prefix);
     }
 
